0be8f18546bc75e7f2b585432dd6dcc050de28a9
swordliongit <kilicarslan.business@gmail.com>
2023-10-27 11:39:00 +0300

    V 0.9.8.2: version attached to firmware, disk usg

--- fmk/rootfs/etc/project_master_modem/vlan.lua
+++ fmk/rootfs/etc/project_master_modem/vlan.lua
@@ -31,14 +31,44 @@ function Vlan.Get_VlanId()
 end
 
 function Vlan.Set_VlanId(vlanId)
-    local vlanPart = "eth1_0." .. vlanId
+    local filename = "/etc/config/network"
+    local file = io.open(filename, "r")
+    if not file then
+        return "Failed to open network configuration file"
+    end
 
-    local cursor = uci.cursor()
-    cursor:foreach("network", "interface", function(s)
-        if s[".name"] == "lan" then
-            s["ifname"] = s["ifname"]:gsub("eth1_0.%d+", vlanPart)
+    local lines = {}
+    for line in file:lines() do
+        -- Modify the 'ifname' line to set the VLAN ID
+        if line:match("option%s+'ifname'") then
+            line = line:gsub("eth1_0.%d+", "eth1_0." .. vlanId)
         end
-    end)
+        table.insert(lines, line)
+    end
+    file:close()
 
-    cursor:commit("network")
+    local newFile = io.open(filename, "w")
+    if not newFile then
+        return "Failed to open network configuration file for writing"
+    end
+
+    for _, line in ipairs(lines) do
+        newFile:write(line, "\n")
+    end
+
+    newFile:close()
 end
+
+-- Didn't work
+-- function Vlan.Set_VlanId(vlanId)
+--     local vlanPart = "eth1_0." .. vlanId
+
+--     local cursor = uci.cursor()
+--     cursor:foreach("network", "interface", function(s)
+--         if s[".name"] == "lan" then
+--             s["ifname"] = s["ifname"]:gsub("eth1_0.%d+", vlanPart)
+--         end
+--     end)
+
+--     cursor:commit("network")
+-- end
